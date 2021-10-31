fe2515211726b0b0175c2d60c627a9b7ee262da6
thara <git@thara.dev>
2021-10-31 21:45:57 +0900

    :muscle: Code Loops

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,11 +5,13 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
-    void Awake() {
-        Transform point = Instantiate(pointPrefab);
-        point.localPosition = Vector3.right;
-
-        point = Instantiate(pointPrefab);
-        point.localPosition = Vector3.right * 2f;
+    void Awake()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
+            point.localScale = Vector3.one / 5f;
+        }
     }
 }
