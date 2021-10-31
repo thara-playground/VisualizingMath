0d68cd5cf75761ab788e8cee19a178ec232d999d
thara <git@thara.dev>
2021-10-31 21:09:05 +0900

    :muscle: Instantiate prefabs

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    void Awake() {
+        Transform point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right;
+
+        point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right * 2f;
+    }
+}
