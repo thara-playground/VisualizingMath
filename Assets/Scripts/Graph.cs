8ba202e64ebde7965170cb779e131bf29989f99e
thara <git@thara.dev>
2021-10-31 21:56:28 +0900

    :muscle: Hoisting the Vectors out of the Loop

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -7,11 +7,14 @@ public class Graph : MonoBehaviour
 
     void Awake()
     {
+        var position = Vector3.zero;
+        var scale = Vector3.one / 5f;
         for (int i = 0; i < 10; i++)
         {
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
-            point.localScale = Vector3.one / 5f;
+            position.x = (i + 0.5f) / 5f - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
         }
     }
 }
