a2366f367d8c006b9ca1afd533c8522a519ee06e
thara <git@thara.dev>
2021-11-03 23:10:47 +0900

    :muscle: Adding Another Dimension

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -19,11 +19,17 @@ public class Graph : MonoBehaviour
         var position = Vector3.zero;
         var scale = Vector3.one * step;
 
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++)
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -38,7 +44,7 @@ public class Graph : MonoBehaviour
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
