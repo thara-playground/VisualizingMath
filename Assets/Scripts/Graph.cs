7acb3652eb3eb0605f729bc7b4e58d6c54da7020
thara <git@thara.dev>
2021-11-03 21:27:19 +0900

    :muscle: Add FunctionLibrary

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,6 +8,9 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
     Transform[] points;
 
     void Awake()
@@ -29,12 +32,13 @@ public class Graph : MonoBehaviour
 
     void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * position.x + time);
+            position.y = f(position.x, time);
             point.localPosition = position;
         }
     }
