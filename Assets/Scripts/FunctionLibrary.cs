714f20635ea278e287744b5909d2828ccf2a9141
thara <git@thara.dev>
2021-11-06 22:05:45 +0900

    :muscle: Three-Dimensional Functions

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -4,7 +4,7 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
-    public delegate float Function(float x, float z, float t);
+    public delegate Vector3 Function(float u, float v, float t);
 
     public enum FunctionName { Wave, MultiWave, Ripple };
 
@@ -15,23 +15,35 @@ public static class FunctionLibrary
         return functions[(int)name];
     }
 
-    public static float Wave(float x, float z, float t)
+    public static Vector3 Wave(float u, float v, float t)
     {
-        return Sin(PI * (x + z + t));
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
     }
 
-    public static float MultiWave(float x, float z, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Sin(PI * (x + 0.5f * t));
-        y += 0.5f * Sin(2f * PI * (z + t));
-        y += Sin(PI * (z + z + 0.25f + t));
-        return y * (1f / 2.5f);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y += Sin(PI * (v + v + 0.25f + t));
+        p.y *= (1f / 2.5f);
+        p.z = v;
+        return p;
     }
 
-    public static float Ripple(float x, float z, float t)
+    public static Vector3 Ripple(float u, float v, float t)
     {
-        float d = Sqrt(x * x + z * z);
-        float y = Sin(PI * (4f * d - t));
-        return y / (1f + 10f * d);
+        Vector3 p;
+        p.x = u;
+        float d = Sqrt(u * u + v * v);
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= (1f + 10f * d);
+        p.z = v;
+        return p;
     }
 }
