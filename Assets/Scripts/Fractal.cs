7df085b871a818c7795b51750b3ccc8084ee2d92
Tomochika Hara <github@thara.dev>
2021-12-19 19:37:23 +0900

    2.6 Animating Again

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -66,18 +66,27 @@ public class Fractal : MonoBehaviour
 
     void Update()
     {
+        Quaternion deltaQuaternion = Quaternion.Euler(0f, 22.5f * Time.deltaTime, 0f);
+
+        FractalPart rootPart = parts[0][0];
+        rootPart.rotation *= deltaQuaternion;
+        rootPart.transform.localRotation = rootPart.rotation;
+        parts[0][0] = rootPart;
+
         for (int li = 1; li < parts.Length; li++) {
             FractalPart[] parentParts = parts[li - 1];
             FractalPart[] levelParts = parts[li];
             for (int fpi = 0; fpi < levelParts.Length; fpi++) {
                 Transform parentTransform = parentParts[fpi / 5].transform;
                 FractalPart part = levelParts[fpi];
+                part.rotation *= deltaQuaternion;
                 part.transform.localRotation =
                     parentTransform.localRotation * part.rotation;
                 part.transform.localPosition =
                     parentTransform.localPosition +
                     parentTransform.localRotation *
                         (1.5f * part.transform.localScale.x * part.direction);
+                levelParts[fpi] = part;
             }
         }
     }
