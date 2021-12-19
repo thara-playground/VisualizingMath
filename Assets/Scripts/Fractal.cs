a3520df25110402f2956a735a153f58be8269b7d
Tomochika Hara <github@thara.dev>
2021-12-19 19:33:13 +0900

    2.5 Rebuilding the Fractal

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Fractal : MonoBehaviour
 {
     struct FractalPart {
-        public Vector3 position;
+        public Vector3 direction;
         public Quaternion rotation;
         public Transform transform;
     }
@@ -38,24 +38,47 @@ public class Fractal : MonoBehaviour
         }
 
         float scale = 1f;
-        CreatePart(0, 0, scale);
+        parts[0][0] = CreatePart(0, 0, scale);
         for (int li = 1; li < parts.Length; li++) {
             scale *= 0.5f;
             FractalPart[] levelParts = parts[li];
             for (int fpi = 0; fpi < levelParts.Length; fpi += 5) {
                 for (int ci = 0; ci < 5; ci++) {
-                    CreatePart(li, ci, scale);
+                    levelParts[fpi + ci] = CreatePart(li, ci, scale);
                 }
             }
         }
     }
 
-    void CreatePart(int levelIndex, int childIndex, float scale)
+    FractalPart CreatePart(int levelIndex, int childIndex, float scale)
     {
         var go = new GameObject("Fractal Part L" + levelIndex + " C" + childIndex);
         go.transform.localScale = scale * Vector3.one;
         go.transform.SetParent(transform, false);
         go.AddComponent<MeshFilter>().mesh = mesh;
         go.AddComponent<MeshRenderer>().material = material;
+        return new FractalPart {
+            direction = directions[childIndex],
+            rotation = rotations[childIndex],
+            transform = go.transform
+        };
+    }
+
+    void Update()
+    {
+        for (int li = 1; li < parts.Length; li++) {
+            FractalPart[] parentParts = parts[li - 1];
+            FractalPart[] levelParts = parts[li];
+            for (int fpi = 0; fpi < levelParts.Length; fpi++) {
+                Transform parentTransform = parentParts[fpi / 5].transform;
+                FractalPart part = levelParts[fpi];
+                part.transform.localRotation =
+                    parentTransform.localRotation * part.rotation;
+                part.transform.localPosition =
+                    parentTransform.localPosition +
+                    parentTransform.localRotation *
+                        (1.5f * part.transform.localScale.x * part.direction);
+            }
+        }
     }
 }
